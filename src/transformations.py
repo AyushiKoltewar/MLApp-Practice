cc583141adc51e6cd00f50c625707e4d86e614f3
Ayushi Koltewar <ayushi.koltewar@fractal.ai>
2026-08-25 16:13:57 +0530

    add null handling in clean data

--- src/transformations.py
+++ src/transformations.py
@@ -2,9 +2,9 @@
 from pyspark.sql import DataFrame
 
 
-def clean_data(df: DataFrame) -> DataFrame:
-    """Clean transaction data."""
-    df = df.filter(df['amount'] > 0)
+def clean_data(df):
+    df = df.na.drop(how='any')
+    df = df.filter(df['amount'].isNotNull())
     return df
 
 
